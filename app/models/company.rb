cd372a9086a28729b6875debe08c2725520ff923
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-11-16 21:13:05 -0300

    add novas classes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_many :transfers
+  has_many :products
+  has_many :sales
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_174243) do
+ActiveRecord::Schema.define(version: 2021_11_17_000343) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.integer "balance_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price_cents"
+    t.integer "company_id"
+    t.integer "category_id"
+    t.integer "brand_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["company_id"], name: "index_products_on_company_id"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "company_id"
+    t.integer "amount_cents"
+    t.integer "product_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_sales_on_company_id"
+    t.index ["product_id"], name: "index_sales_on_product_id"
   end
 
   create_table "transfers", force: :cascade do |t|
